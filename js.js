0f8d5e5ca8d85f1444b24623feb05c4e14c2d5ab
Renz Mallari <hmmwow2015@gmail.com>
2016-11-23 20:51:02 -0500

    added html, and css files

---
+++ js.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.myname').show(1000);
+
+	$('.iam').slideDown(1500);
+	$('.at').slideDown(1800);
+	$('.mycamp').slideDown(2000);
+	$('#pro-name').show(1000);
+
+
+});
